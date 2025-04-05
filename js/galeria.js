21b0caff2a1834a33db7518f0fe2c9bd4072d4b6
claudia6666 <167026773+claudia6666@users.noreply.github.com>
2025-04-05 20:42:20 +0200

    fin

---
+++ js/galeria.js
@@ -0,0 +1,6 @@
+
+function mostrarSeccion(id) {
+    const secciones = document.querySelectorAll('.seccion');
+    secciones.forEach(sec => sec.classList.remove('activa'));
+    document.getElementById(id).classList.add('activa');
+}
